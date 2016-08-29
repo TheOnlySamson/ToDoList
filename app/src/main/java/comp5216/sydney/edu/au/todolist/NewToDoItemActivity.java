aa2af71fd3b76a07ca72ed3e43e719a685cab05b
Samson Jabin <Samson Jabin>
2016-08-29 14:09:38 +1000

    Initial commit

---
+++ app/src/main/java/comp5216/sydney/edu/au/todolist/NewToDoItemActivity.java
@@ -0,0 +1,48 @@
+package comp5216.sydney.edu.au.todolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NewToDoItemActivity extends AppCompatActivity {
+
+    EditText editText;
+    FloatingActionButton submit;
+    MainActivity mainActivity = new MainActivity();
+    String msg;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.edit_add_item);
+
+        editText = (EditText) findViewById(R.id.add_item_sub);
+
+////        // Prepare data intent for sending it back
+//        Intent data = new Intent();
+////
+////        // Pass relevant data back as a result
+//        data.putExtra("item", msg);
+//        data.putExtra("position", "354654");
+////
+////
+////        // Activity finished ok, return the data
+//        setResult(RESULT_OK, data); // set result code and bundle data for response
+//        finish(); // closes the activity, pass data to parent
+
+    }
+
+    public void onNewItemSubmit(View view){
+        msg = editText.getText().toString();
+        Toast.makeText(NewToDoItemActivity.this, msg, Toast.LENGTH_LONG).show();
+        //mainActivity.setupList(msg);
+        startActivity(new Intent(NewToDoItemActivity.this, MainActivity.class));
+    }
+
+}
